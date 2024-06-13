fe5cd65b29df03f46a470d2eb96f4822cca0cd01
mohammadsoheluddin <mohammadsohel342@yahoo.com>
2024-06-13 15:39:06 +0600

    added a point into project-self-analysis.txt file

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -21,7 +21,19 @@ const findLastStudentId = async () => {
 
 export const generateStudentId = async (payload: TAcademicSemester) => {
   // first time 0000
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString();
+  const lastStudentId = await findLastStudentId();
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const lastStudentYear = lastStudentId?.substring(0, 4);
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year;
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
   incrementId = `${payload.year}${payload.code}${incrementId}`;
   return incrementId;
